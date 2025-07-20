d48d1c7d545eb0256d4c9871254ac252a477273d
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-07-20 22:29:46 +0100

    Add focus state to amount input

--- src/components/InputAmount.jsx
+++ src/components/InputAmount.jsx
@@ -6,12 +6,12 @@ export default function InputAmount() {
       </label>
       <div>
         <input
-          className="w-full h-12.5 border-1 decoration-solid decoration-slate-500 rounded-sm pl-13"
+          className="peer w-full h-12.5 border-0 rounded-sm pl-13 outline outline-slate-500 focus-visible:outline-2 focus-visible:outline-lime cursor-pointer"
           type="text"
           id="mortgage-amount"
           name="amount"
         />
-        <span className="relative block before:bg-slate-100 before:absolute before:w-11 before:h-12 before:content-['\00A3'] before:left-[1px] before:bottom-[1px] before:grid before:place-items-center before:rounded-l-[.1875rem] before:font-bold before:text-slate-700"></span>
+        <span className="relative block before:bg-slate-100 before:absolute before:w-11 before:h-12 before:content-['\00A3'] before:left-[1px] before:bottom-[1px] before:grid before:place-items-center before:rounded-l-[.1875rem] before:font-bold before:text-slate-700 peer-focus-visible:[&::before]:bg-lime peer-focus-visible:[&::before]:text-slate-900"></span>
       </div>
     </div>
   );
