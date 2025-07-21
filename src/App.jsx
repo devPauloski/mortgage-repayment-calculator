06989c02956725fc85b3e0f78567879039b6578b
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-07-21 23:08:59 +0100

    Restructure code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import FormWrapper from "./components/FormWrapper.jsx";
 import FormInputs from "./components/FormInputs.jsx";
-import FormOutputs from "./components/FormOutputs.jsx";
 import Header from "./components/Header.jsx";
 import InputAmount from "./components/InputAmount.jsx";
 import InputTerm from "./components/InputTerm.jsx";
 import InputRate from "./components/InputRate.jsx";
 import MortgageOptions from "./components/MortgageOptions.jsx";
 import SubmitButton from "./components/SubmitButton.jsx";
+import FormOutputs from "./components/FormOutputs.jsx";
 
 export default function App() {
   const [amount, setAmount] = useState("");
