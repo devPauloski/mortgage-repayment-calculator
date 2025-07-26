350524c2a1aa0422092f3f1799fa5973d74e7a9b
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-07-26 16:19:51 +0100

    Makes changes

--- src/components/FormOutputs.jsx
+++ src/components/FormOutputs.jsx
@@ -3,7 +3,7 @@ import OutputResults from "./OutputResults";
 
 export default function FormOutputs({ results }) {
   return (
-    <section className="grid bg-slate-900 px-4 py-8 text-slate-300 md:rounded-tr-2xl md:rounded-br-2xl md:rounded-bl-[4rem]">
+    <section className="grid bg-slate-900 px-4 py-8 text-slate-300 md:rounded-tr-2xl md:rounded-br-2xl md:rounded-bl-[4rem] md:px-8">
       {!results ? <OutputEmpty /> : <OutputResults results={results} />}
     </section>
   );
--- src/components/OutputResults.jsx
+++ src/components/OutputResults.jsx
@@ -1,7 +1,7 @@
 export default function OutputResults({results}) {
   return (
     <div>
-      <h2>Your results</h2>
+      <h2 className="text-white text-[1.375rem]">Your results</h2>
       <p>
         Your results are shown below based on the information you provided. To
         adjust the results, edit the form and click “calculate repayments”
@@ -9,11 +9,11 @@ export default function OutputResults({results}) {
       </p>
       <p>
         <span>Your monthly repayments</span>
-        <output>{results.monthly}</output>
+        <span>&pound;{results.monthly}</span>
       </p>
       <p>
         <span>Total you'll repay over the term</span>
-        <output>{results.total}</output>
+        <span>&pound;{results.total}</span>
       </p>
     </div>
   );
