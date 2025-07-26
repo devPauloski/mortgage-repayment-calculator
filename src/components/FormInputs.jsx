b03630f8362d46d51dc38a44b6bb8e74fd4bb15d
Paul Mgbechi <mpaulchijioke@gmail.com>
2025-07-26 18:48:35 +0100

    Makes changes

--- src/components/FormInputs.jsx
+++ src/components/FormInputs.jsx
@@ -1,6 +1,6 @@
 export default function FormInputs({children}) {
   return (
-    <section className="px-[5%] py-7 *:not-last:mb-4.5">
+    <section className="px-[5%] py-7 md:px-8 md:py-8 *:not-last:mb-6.5">
       {children}
     </section>
   );
